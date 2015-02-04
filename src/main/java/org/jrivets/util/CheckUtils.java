c7cf6f1ac0552f61a466d5bc828e5357198ed3c6
Dmitry Spasibenko <dmitry.spasibenko@mulesoft.com>
2015-02-04 01:19:27 -0800

    check for nulls

--- src/main/java/org/jrivets/util/CheckUtils.java
+++ src/main/java/org/jrivets/util/CheckUtils.java
@@ -8,4 +8,27 @@ public final class CheckUtils extends StaticSingleton {
         } 
     }
     
+    public static void notNull(Object o1) {
+        if (o1 == null) {
+            throw new NullPointerException();
+        }
+    }
+    
+    public static void notNull(Object o1, Object o2) {
+        if (o1 == null || o2 == null) {
+            throw new NullPointerException();
+        }
+    }
+    
+    public static void notNull(Object o1, Object o2, Object o3) {
+        if (o1 == null || o2 == null || o3 == null) {
+            throw new NullPointerException();
+        }
+    }
+    
+    public static void notNull(Object o1, Object o2, Object o3, Object o4) {
+        if (o1 == null || o2 == null || o3 == null || o4 == null) {
+            throw new NullPointerException();
+        }
+    }
 }
